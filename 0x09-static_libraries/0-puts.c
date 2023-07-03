76d82939df9e4040ac17615d8489486e94f1eb14
safaanadif <nadifs@hotmail.fr>
2023-07-03 14:44:57 -0700

    libmy.

---
+++ 0x09-static_libraries/0-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - Prints a string to stdout
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+}
